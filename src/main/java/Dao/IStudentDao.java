654106c8fec2bee1ed71bdc102c946f0ca439ce1
Zhenli He <hezl@ynu.edu.cn>
2021-10-16 00:02:21 +0800

    测试修改

--- src/main/java/Dao/IStudentDao.java
+++ src/main/java/Dao/IStudentDao.java
@@ -2,7 +2,7 @@ package Dao;
 import Entity.Student;
 import java.util.List;
 
-public interface IStudentDao {
+public interface IStudentDao  {
     void save(Student s);
     void update(Student s);
     void delete(Long Id);
